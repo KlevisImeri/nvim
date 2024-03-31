2101305241bad41f13f29c8a4128153f40c5b663
root <root@DESKTOP-K26KADK.localdomain>
2024-03-31 13:58:55 +0200

    changes

--- lua/lazy/plugins/nightfly.lua
+++ lua/lazy/plugins/nightfly.lua
@@ -2,7 +2,7 @@ return {
 	"bluz71/vim-nightfly-colors",
 	name = "nightfly",
 	priority = 1000,
-	-- config = function()
-	-- 	vim.cmd.colorscheme 'nightfly'
-	-- end,
+	config = function()
+		vim.cmd.colorscheme 'nightfly'
+	end,
 }
