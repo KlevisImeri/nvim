b75ea00ec8d3ac20270f59e7f4d0d7fd220d885f
Klevis Imeri <klevisimeri11@gmail.com>
2025-03-11 14:22:34 +0100

    More advance pasteimg

--- lua/lazy/plugins/imagepaste.lua
+++ lua/lazy/plugins/imagepaste.lua
@@ -1,8 +1,8 @@
 return {
-  'img-paste-devs/img-paste.vim',
-  config = function()
-    vim.g.mdip_imgdir = 'img'
-    vim.g.mdip_imgname = 'image'
-    vim.cmd [[autocmd FileType markdown nmap <buffer><silent> <leader>p :call mdip#MarkdownClipboardImage()<CR>]]
-  end
+  "HakonHarnes/img-clip.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+  },
 }
