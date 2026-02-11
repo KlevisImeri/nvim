5158d5bd06f249a5413128caa274221601a83a40
Klevis Imeri <klevisimeri11@gmail.com>
2026-02-11 12:45:10 +0100

    Added aw-watcher

---
+++ lua/lazy/plugins/aw-watcher.lua
@@ -0,0 +1,15 @@
+{
+  "lowitea/aw-watcher.nvim",
+  opts = { 
+    bucket = {
+      hostname = nil,
+      name = nil,
+    },
+    aw_server = {
+      host = "127.0.0.1",
+      port = 5600,
+      ssl_enable = false,
+      pulsetime = 30,
+    },
+  },
+}
