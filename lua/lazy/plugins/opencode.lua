72d557fe98cba3947a6614b9982e40d5c0b02f54
Klevis Imeri <klevisimeri11@gmail.com>
2026-04-02 20:13:02 +0200

    [Fixed] lag when pressed spaced in terminal mode

--- lua/lazy/plugins/opencode.lua
+++ lua/lazy/plugins/opencode.lua
@@ -34,7 +34,7 @@ return {
 
     vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
     vim.keymap.set({ "n", "x" }, "<leader>oo", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "t" }, "<leader>ot", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+    vim.keymap.set({ "n" }, "<leader>ot", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
 
     vim.keymap.set({ "n", "x" }, "<leader>or", function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
     vim.keymap.set("n",          "<leader>ol", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
