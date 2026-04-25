5d861db824e94aaf7512f4b53269236567f29c11
Klevis Imeri <klevisimeri11@gmail.com>
2026-04-25 18:28:10 +0200

    Remove opencode plugin

--- lua/lazy/plugins/treesitter.lua
+++ lua/lazy/plugins/treesitter.lua
@@ -6,8 +6,8 @@ return {
     require("nvim-treesitter").setup {}
 
     require("nvim-treesitter").install {
-      "bash", "c", "cpp", "c_sharp", "html", "java", "json", "kotlin", "lua", "markdown",
-      "vim", "vimdoc", "go", "rust", "tsx", "typescript", "vue",
+      "bash", "c", "cpp", "c_sharp", "html", "java", "javascript", "jinja2", "json", "kotlin",
+      "lua", "markdown", "tsx", "typescript", "vim", "vimdoc", "go", "rust", "vue",
     }
 
     vim.api.nvim_create_autocmd("FileType", {
