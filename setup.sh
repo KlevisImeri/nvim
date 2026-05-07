d25d03cc083574031204dde0bd42361b16ec69a1
Klevis Imeri <klevisimeri11@gmail.com>
2026-05-07 22:05:46 +0200

    Add tree-sitter CLI installation to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
 
 sudo dnf install -y git ripgrep fd-find make gcc firefox
+
+curl -LO https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-linux-x64.gz
+gunzip tree-sitter-linux-x64.gz
+chmod +x tree-sitter-linux-x64
+sudo mv tree-sitter-linux-x64 /usr/local/bin/tree-sitter
