11debbf9e883f10436707335a726533b8a95ff31
Klevis Imeri <klevisimeri11@gmail.com>
2026-05-17 20:47:41 +0200

    compile-mode.nvim config fix

--- lua/lazy/plugins/compiler-mode.lua
+++ lua/lazy/plugins/compiler-mode.lua
@@ -1,35 +1,54 @@
 return {
   "ej-shafran/compile-mode.nvim",
-  -- version = "^5.0.0",
-  -- you can just use the latest version:
+  dir = "~/Projects/compile-mode.nvim",
   branch = "latest",
-  -- or the most up-to-date updates:
-  -- branch = "nightly",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    -- if you want to enable coloring of ANSI escape codes in
-    -- compilation output, add:
     { "m00qek/baleia.nvim" },
   },
   config = function()
     ---@type CompileModeOpts
-    vim.g.compile_mode = {
-        baleia_setup = true,
+    vim.g.compile_mode = function()
+      return {
+        ansi_color_for_compilation = "render",
+        baleia_setup = {
+          colors = setmetatable({}, {
+            __index = function(_, i)
+              return vim.g["terminal_color_" .. i]
+            end,
+          }),
+        },
         bang_expansion = true,
-        -- debug = true,
-
         environment = {
-            CARGO_TERM_COLOR = "always",
+          CARGO_TERM_COLOR = "always",
         },
-
         error_regexp_table = {
-            rust = {
-                regex = "^ *--> \\([^:]\\+\\):\\([0-9]\\+\\):\\([0-9]\\+\\)",
-                filename = 1,
-                row = 2,
-                col = 3,
-            },
+          rust = {
+            regex = "^ *--> \\([^:]\\+\\):\\([0-9]\\+\\):\\([0-9]\\+\\)",
+            filename = 1,
+            row = 2,
+            col = 3,
+          },
         },
+      }
+    end
+
+    local groups = {
+      { "CompileModeError",           { fg = "#e06c75" } },
+      { "CompileModeWarning",         { fg = "#e5c07b" } },
+      { "CompileModeInfo",            { fg = "#56b6c2" } },
+      { "CompileModeMessage",         { link = "Normal" } },
+      { "CompileModeCommandOutput",   { fg = "#abb2bf" } },
+      { "CompileModeMessageRow",      { fg = "#c678dd" } },
+      { "CompileModeMessageCol",      { fg = "#c678dd" } },
+      { "CompileModeDirectoryMessage",{ fg = "#56b6c2" } },
+      { "CompileModeCheckTarget",     { fg = "#abb2bf" } },
+      { "CompileModeCheckResult",     { link = "Normal" } },
+      { "CompileModeOutputFile",      { fg = "#56b6c2" } },
+      { "CompileModeErrorLocus",      { bg = "#3e4451" } },
     }
-  end
+    for _, item in ipairs(groups) do
+      vim.api.nvim_set_hl(0, item[1], item[2])
+    end
+  end,
 }
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo dnf remove -y neovim
+
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
